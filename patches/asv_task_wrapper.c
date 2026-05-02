b55ac9f8afc1e0bc5d4e8ef6f6067bd2e97e8a50
Topher Winward <winwardo@gmail.com>
2026-05-02 22:29:06 +0100

    Revert random changes

--- patches/asv_task_wrapper.c
+++ patches/asv_task_wrapper.c
@@ -1,21 +1,15 @@
 // Author: noombs @ Discord
 
-// float * const fvars = (void*) 0x2000e948;
-// int * const ivars = (void*) 0x2000e750;
+extern int isNotBreathingPtr;
+extern float breathPercentagePtr;
+extern void asv_task_function(void);
 
 void start() {
     // Suppress the ASV backup rate
-    //
-    int* isNotBreathingPtr = (int*)0x2000e808;
-    float* breathPercentagePtr = (float*)0x2000ea9c;
-    // float* breathPercentagePtr = &fvars[0xD3]
-
-    if (*isNotBreathingPtr == 1 && *breathPercentagePtr > 0.98f) {
-        *breathPercentagePtr = 0.98f;
+    if (isNotBreathingPtr == 1 && breathPercentagePtr > 0.98f) {
+        breathPercentagePtr = 0.98f;
     }
 
     // Execute the ASV task
-    //
-    void (*asvFunctionPtr)() = (void*)0x80e3465;
-    asvFunctionPtr();
+    asv_task_function();
 }
